a4957abbe62c8c164fb1d908f7942f104789bda1
James Desktop <jtc20659@gmail.com>
2024-12-15 15:51:34 -0500

    Updated some doxygen, added Weighted Graphs and implemented a form of Dijkstras algorithm

--- Graphs.cs
+++ Graphs.cs
@@ -8,6 +8,8 @@ namespace Mazes
      * Given this relationship, we can create a structure that can later be visualized and implemented for other purposes.
      * Some Uses would be Mazes: Both Generation and Solving, as well as relational pathing between 2 objects.
      * Implements IEnumerable in order to be able to loop through the vertices.
+     * @see DirectionalGraph
+     * @see WeightedGraph
      ***********************************************************/
     public class Graph<T> : IEnumerable<T> where T : notnull
     {
@@ -80,10 +82,11 @@ namespace Mazes
     /***********************************************************
      * a directional graph implementation based on the Graph Class.
      * @see Graph
+     * @see WeightedGraph
      * A directional graph implementation does not reciprocate an edge between vertices, thus giving 
-     * a direction in which a vertex is a neighbor to another. The rest of the implementation would stay the same
+     * a direction in which a vertex is a neighbor to another. The rest of the implementation would stay the same.
      ***********************************************************/
-    public class Digraph<T> : Graph<T> where T : notnull
+    public class DirectionalGraph<T> : Graph<T> where T : notnull
     {
         /**
           Add an Edge from u to v, but not v to u
@@ -97,6 +100,167 @@ namespace Mazes
             {
                 this._neighbors[u].Add(v);
             }
+        }
+    }
+
+    /***********************************************************
+     * a weighted graph implementation based on the Graph Class.
+     * @see Graph
+     * @see WeightedDirectionalGraph
+     * A weighted Graph assigns a weight to an edge. This weight can be used by algorithms to find the most opimal paths, such as Dijkstra's or A* algorithms.
+     ***********************************************************/
+    public class WeightedGraph<T> : IEnumerable<T> where T : notnull
+    {
+        /**
+         a Dictionary type where the key is a vertex and the value is it's list of neighbors.
+         */
+        protected readonly Dictionary<T, List<KeyValuePair<T, int>>> _neighbors;
+        public WeightedGraph()
+        {
+            this._neighbors = new Dictionary<T, List<KeyValuePair<T, int>>>();
+        }
+
+        /**
+         * Add a vertex to the map, default initialized with no neighbors.
+         * @param v the value of the vertex we wish to add.
+         */
+        public void AddVertex(T v)
+        {
+            if (!this._neighbors.ContainsKey(v))
+            {
+                this._neighbors.Add(v, new List<KeyValuePair<T, int>>());
+            }
+        }
+
+        /**
+          Add an Edge between u to v.
+        Note: Will only add an edge if both vertices already exist.
+        @see AddVertex(T v)
+        @param u the vertex that you wish to add an edge to.
+        @param v the other vertex that you wish to add an edge to.
+        @param weight the given weight of said edge.
+         */
+        public void AddEdge(T u, T v, int weight)
+        {
+            if (this._neighbors.ContainsKey(u) && this._neighbors.ContainsKey(v))
+            {
+                this._neighbors[u].Add(new KeyValuePair<T, int>(v, weight));
+                this._neighbors[v].Add(new KeyValuePair<T, int>(u, weight));
+            }
+        }
+
+        /**
+         * Get the list of neighbors of a given vertex
+         * @returns the list of neighbors for a vertex if the vertex exists with their accompanying weights, else an empty list
+         * @param v the vertex we wish to get the neighbors for.
+         */
+        public List<KeyValuePair<T, int>> getNeighbors(T v)
+        {
+            if (this._neighbors.ContainsKey(v))
+            {
+                return this._neighbors[v];
+            }
+            return new List<KeyValuePair<T, int>>();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (T v in this._neighbors.Keys)
+            {
+                yield return v;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+    }
+
+    /***********************************************************
+     * a weighted directional graph implementation based on the Graph Class.
+     * @see DirectionalGraph
+     * @see WeightedGraph
+     * A directional graph implementation does not reciprocate an edge between vertices, thus giving 
+     * a direction in which a vertex is a neighbor to another. The rest of the implementation would stay the same.
+     ***********************************************************/
+    public class WeightedDirectionalGraph<T> : WeightedGraph<T> where T : notnull
+    {
+        /**
+          Add an Edge from u to v, but not v to u
+
+        @param u the vertex that you wish to add an edge to.
+        @param v the vertex that will have an edge pointed to it.
+        @param weight the given weight of said edge.
+         */
+        public new void AddEdge(T u, T v, int weight)
+        {
+            if (this._neighbors.ContainsKey(u) && this._neighbors.ContainsKey(v))
+            {
+                this._neighbors[u].Add(new KeyValuePair<T, int>(v, weight));
+            }
+        }
+    }
+
+    /***********************************************************
+    * a weighted graph implementation based on the Graph Class forcing non-negative weights.
+    * @see WeightedGraph
+    * A weighted Graph assigns a weight to an edge. This weight can be used by algorithms to find the most opimal paths, such as Dijkstra's or A* algorithms.
+    * Forcing non negative weights makes sure that we can always use Dijkstras
+    ***********************************************************/
+    public class NonNegativeWeightedGraph<T> : WeightedGraph<T> where T : notnull
+    {
+        /**
+         a Dictionary type where the key is a vertex and the value is it's list of neighbors.
+         */
+        protected new readonly Dictionary<T, List<KeyValuePair<T, uint>>> _neighbors;
+        public NonNegativeWeightedGraph()
+        {
+            this._neighbors = new Dictionary<T, List<KeyValuePair<T, uint>>>();
+        }
+
+        /**
+         * Add a vertex to the map, default initialized with no neighbors.
+         * @param v the value of the vertex we wish to add.
+         */
+        public new void AddVertex(T v)
+        {
+            if (!this._neighbors.ContainsKey(v))
+            {
+                this._neighbors.Add(v, new List<KeyValuePair<T, uint>>());
+            }
+        }
+
+        /**
+          Add an Edge between u to v.
+        Note: Will only add an edge if both vertices already exist.
+        @see AddVertex(T v)
+        @param u the vertex that you wish to add an edge to.
+        @param v the other vertex that you wish to add an edge to.
+        @param weight the given weight of said edge.
+         */
+        public void AddEdge(T u, T v, uint weight)
+        {
+            if (this._neighbors.ContainsKey(u) && this._neighbors.ContainsKey(v))
+            {
+                this._neighbors[u].Add(new KeyValuePair<T, uint>(v, weight));
+                this._neighbors[v].Add(new KeyValuePair<T, uint>(u, weight));
+            }
+        }
+
+        /**
+         * Get the list of neighbors of a given vertex
+         * @returns the list of neighbors for a vertex if the vertex exists with their accompanying weights, else an empty list
+         * @param v the vertex we wish to get the neighbors for.
+         */
+        public new List<KeyValuePair<T, uint>> getNeighbors(T v)
+        {
+            if (this._neighbors.ContainsKey(v))
+            {
+                return this._neighbors[v];
+            }
+            return new List<KeyValuePair<T, uint>>();
         }
     }
 }
--- SearchAlgorithmExtensions.cs
+++ SearchAlgorithmExtensions.cs
@@ -1,4 +1,5 @@
-﻿namespace Mazes
+﻿
+namespace Mazes
 {
     /***********************************************************
      * An extension class for Graph that implements searching algorithms as well as their complementary paths.
@@ -16,8 +17,12 @@
          * @param start the starting vertex that we will begin the search from.
          * @returns a set of all vertexes found that have a path from start.
          */
-    public static ISet<T> DFS<T>(this Graph<T> graph, T start)
-        {
+    public static ISet<T> DFS<T>(this Graph<T> graph, T start) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+
             ISet<T> found = new HashSet<T>();
             Stack<T> waiting = new Stack<T>();
             found.Add(start);
@@ -47,8 +52,12 @@
          * @param start the starting vertex that we will begin the search from.
          * @returns a set of all vertexes found that have a path from start.
          */
-        public static ISet<T> BFS<T>(this Graph<T> graph, T start)
-        {
+        public static ISet<T> BFS<T>(this Graph<T> graph, T start) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+
             ISet<T> found = new HashSet<T>();
             Queue<T> waiting = new Queue<T>();
             found.Add(start);
@@ -69,53 +78,6 @@
         }
 
         /**
-         * A Breadth First Search Algorithm to find a path between two vertices.
-         * Given a starting value, discover the possible paths and routes available from the edges until we find a path between the start and end.
-         * @see BFS<T>(this Graph<T> graph, T start)
-         * @param graph since this is an extension, graph would be where we call this function to run on that specific graph instance.
-         * @param start the starting vertex that we will begin the search from.
-         * @param end the ending vertex that we are looking for.
-         * @returns a list format of the found path between start and end.
-         */
-        public static List<T> BFSPath<T>(this Graph<T> graph, T start, T end)
-        {
-            ISet<T> found = new HashSet<T>();
-            Queue<T> waiting = new Queue<T>();
-            Dictionary<T, T> parent = new Dictionary<T, T>();
-            found.Add(start);
-            waiting.Enqueue(start);
-            parent.Add(start, default);
-            while (waiting.Count > 0)
-            {
-                T v = waiting.Dequeue();
-                T u;
-                List<T> neighbors = graph.getNeighbors(v);
-                for (int i = 0; i < neighbors.Count; i++) 
-                {
-                    u = neighbors[i];
-                    if (!found.Contains(u))
-                    {
-                        found.Add(u);
-                        waiting.Enqueue(u);
-                        parent.Add(u, v);
-                    }
-                    if (!u.Equals(end))
-                    {
-                        List<T> path = new List<T>();
-                        while (u.Equals(default))
-                        {
-                            path.Add(u);
-                            u = parent[u];
-                        }
-                        path.Reverse();
-                        return path;
-                    }
-                }
-            }
-            return new List<T>();
-        }
-
-        /**
          * A Depth First Search Algorithm to find a path between two vertices.
          * Given a starting value, discover the possible paths and routes available from the edges until we find a path between the start and end.
          * @see DFS<T>(this Graph<T> graph, T start)
@@ -124,8 +86,12 @@
          * @param end the ending vertex that we are looking for.
          * @returns a list format of the found path between start and end.
          */
-        public static List<T> DFSPath<T>(this Graph<T> graph, T start, T end)
-        {
+        public static List<T> DFSPath<T>(this Graph<T> graph, T start, T end) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+
             ISet<T> found = new HashSet<T>();
             Stack<T> waiting = new Stack<T>();
             Dictionary<T, T> parent = new Dictionary<T, T>();
@@ -146,7 +112,7 @@
                         waiting.Push(u);
                         parent.Add(u, v);
                     }
-                    if (!u.Equals(end))
+                    if (u.Equals(end))
                     {
                         List<T> path = new List<T>();
                         while (!u.Equals(default))
@@ -162,5 +128,170 @@
             return new List<T>();
         }
 
+        /**
+         * A Breadth First Search Algorithm to find a path between two vertices.
+         * Given a starting value, discover the possible paths and routes available from the edges until we find a path between the start and end.
+         * @see BFS<T>(this Graph<T> graph, T start)
+         * @param graph since this is an extension, graph would be where we call this function to run on that specific graph instance.
+         * @param start the starting vertex that we will begin the search from.
+         * @param end the ending vertex that we are looking for.
+         * @returns a list format of the found path between start and end.
+         */
+        public static List<T> BFSPath<T>(this Graph<T> graph, T start, T end) where T: notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+
+            ISet<T> found = new HashSet<T>();
+            Queue<T> waiting = new Queue<T>();
+            Dictionary<T, T> parent = new Dictionary<T, T>();
+            found.Add(start);
+            waiting.Enqueue(start);
+            parent.Add(start, default);
+            while (waiting.Count > 0)
+            {
+                T v = waiting.Dequeue();
+                T u;
+                List<T> neighbors = graph.getNeighbors(v);
+                for (int i = 0; i < neighbors.Count; i++)
+                {
+                    u = neighbors[i];
+                    if (!found.Contains(u))
+                    {
+                        found.Add(u);
+                        waiting.Enqueue(u);
+                        parent.Add(u, v);
+                    }
+                    if (u.Equals(end))
+                    {
+                        List<T> path = new List<T>();
+                        while (!u.Equals(default))
+                        {
+                            path.Add(u);
+                            u = parent[u];
+                        }
+                        path.Reverse();
+                        return path;
+                    }
+                }
+            }
+            return new List<T>();
+        }
+    }
+
+
+    /***********************************************************
+     * An extension class for WeightedGraph that implements greedy algorithms as well as their complementary paths.
+     * @see WeightedGraph
+     ***********************************************************/
+    public static class GreedyAlgorithmExtensions
+    {
+        /**
+         * Implementation of Dijkstra's algorithm to return the shortest path from start to end.
+         * 
+         * This is not the most efficient algorithm, nor the most efficient implementation of the algorithm. The main issue is that for any start and end we will have to do the distance mapping step again.
+         * One way to make this more time efficient for larger programs is to encapsulate a cache class.
+         * Whenever a start is first called to be used in Dijkstras, we save the parent map so that from any endpoint we just have to follow back the trace.
+         * This would be better in the long run, as long as any later addEdge or addVertex call either deletes this cache or updates it.
+         * 
+         * An important note: Dijkstras does not work with negative weights, so we have to enforce the weights being non-negative
+         * @see NonNegativeWeightedGraph
+         * @param graph since this is an extension, graph would be where we call this function to run on that specific graph instance.
+         * @param start the starting vertex that we will begin the search from.
+         * @param end the ending vertex that we are looking for.
+         * @returns a list format of the found path between start and end, if any
+         */
+        public static List<T> Dijkstra<T>(this NonNegativeWeightedGraph<T> graph, T start, T end) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+            int count = graph.Count();
+
+            Dictionary<T, uint> distances = new Dictionary<T, uint>();
+            foreach (var node in graph)
+            {
+                distances[node] = uint.MaxValue; // Logical infinity
+            }
+            if (!distances.ContainsKey(end))
+            {
+                return new List<T>(); // Get out before we waste time, end doesn't even exist in the graph
+            }
+            ISet<T> found = new HashSet<T>(); // What Nodes we found
+            PriorityQueue<T, uint> waiting = new PriorityQueue<T, uint>(); // Waiting queue that we will pop as we can, the given weight determines order
+            Dictionary<T, T> parent = new Dictionary<T, T>(); // How we will trace back from a node for the path
+
+            //First step: Add start
+            found.Add(start);
+            waiting.Enqueue(start, 0);
+            parent.Add(start, default);
+
+            // Distance Building Step: Go through every node that we can possibly reach from start, even if we find end
+            while (waiting.Count > 0)
+            {
+                T v = waiting.Dequeue();
+                var neighbors = graph.getNeighbors(v);
+                for (int i = 0; i < neighbors.Count; i++)
+                {
+                    var neighbor = neighbors[i];
+                    if (neighbor.Value + distances[v] < distances[neighbor.Key])
+                    {
+                        parent.Add(neighbor.Key, v); // If this would actually be the shortest path
+                        distances[neighbor.Key] = neighbor.Value + distances[v]; // updating the distances
+                    }
+                    if (!found.Contains(neighbor.Key))
+                    {
+                        found.Add(neighbor.Key);
+                        waiting.Enqueue(neighbor.Key, neighbor.Value);
+                        parent.Add(neighbor.Key, v);
+                    }
+                }
+            }
+
+            // Pathing step: Now that we have the distances, return the path if there is one.
+            if (distances[end] == uint.MaxValue)
+            {
+                return new List<T>(); // Empty list: end is completely unreachable
+            }
+            List<T> ans = new List<T>();
+            ans.Add(end);
+            T u = parent[end];
+            while (!u.Equals(default))
+            {
+                ans.Add(u);
+                u = parent[u];
+            }
+            ans.Reverse();
+            return ans;
+        }
+
+        public static void BellmanFord<T>(this WeightedGraph<T> graph, T start, T end) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+        }
+
+        public static void FloydWarshall<T>(this WeightedGraph<T> graph, T start, T end) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+        }
+
+        public static void AStar<T>(this WeightedGraph<T> graph, T start, T end) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+        }
+
+        public static void AStar<T>(this Graph<T> graph, T start, T end) where T : notnull {
+            if (graph is null)
+            {
+                throw new ArgumentNullException(nameof(graph));
+            }
+        }
     }
 }
