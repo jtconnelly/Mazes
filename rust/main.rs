a61ce36cee226441a74d97b12d6e0a0133a9a00a
jtconnelly <jtc20659@gmail.com>
2025-11-16 15:53:30 -0500

    Rust: Added bfs and dfs for anything implementing Graphing, added impl call for Graph since that was already added

--- rust/main.rs
+++ rust/main.rs
@@ -1,8 +1,29 @@
 use crate::graph::{Graph, Graphing};
+use crate::searching::SearchAlgorithm;
 
 mod graph;
+mod searching;
+
+fn test_searching(graph: &Graph<char>){
+    println!("test_searching function");
+
+    println!("DFS");
+    let dfs_res = graph.dfs('a', 'd');
+    assert!(!dfs_res.is_empty());
+    let dfs_str = format!("{:?}", dfs_res);
+    println!("DFS Result: {dfs_str}");
+
+    println!();
+
+    println!("BFS");
+    let bfs_res = graph.bfs('a', 'd');
+    assert!(!bfs_res.is_empty());
+    let bfs_str = format!("{:?}", bfs_res);
+    println!("BFS Result: {bfs_str}");
+}
 
 fn test_graph(){
+    println!("test_graph function");
     let mut graph = Graph::<char>::new();
     graph.add_vertex('a');
     graph.add_vertex('b');
@@ -13,14 +34,21 @@ fn test_graph(){
     graph.remove_vertex('x');
     graph.add_edge('a', 'b');
     graph.add_edge('a', 'c');
+    graph.add_edge('b', 'd');
+    graph.add_edge('d', 'c');
+    graph.add_edge('d', 'e');  
+    graph.add_edge('a', 'b');
+    graph.add_edge('a', 'c');
     graph.add_edge('a', 'd');
     graph.add_edge('a', 'b');
     assert_eq!(*graph.get_neighbors('a').unwrap(), vec!['b', 'c', 'd']);
     graph.remove_edge('a', 'b');
     assert_eq!(*graph.get_neighbors('a').unwrap(), vec!['c', 'd']);
     assert!(true, "get_neighbors failed!");
+    test_searching(&graph);
 }
 
 fn main() {
+    println!("Testing Graph Class");
     test_graph();
 }
--- rust/searching.rs
+++ rust/searching.rs
@@ -1,12 +1,70 @@
-mod result;
-mod graph;
-
-pub trait search_algorithm<T>
+use crate::graph::{Graph, Graphing};
+use std::hash::Hash;
+pub trait SearchAlgorithm<T: Clone + Eq + Hash>: Graphing<T>
 {
-    fn dfs(self: &impl graph) -> Vec<T>;
-    fn bfs() -> Vec<T>;
-    fn dijkstra() -> Vec<T>;
-    fn bellman_ford() -> Vec<T>;
-    fn floyd_warshall() -> Vec<T>;
-    fn a_star() -> Vec<T>;
+    fn bfs(&self, start: T, end: T) -> Vec<T>{
+        let mut path: Vec<T> = Vec::new();
+        let mut stack: Vec<T> = Vec::new();
+        stack.push(start.clone());
+
+        while !stack.is_empty()
+        {
+            let node = stack.remove(0);
+
+            if node == end
+            {
+                path.push(node);
+                return path;
+            }
+
+            if path.iter().find(|&x| *x == node).is_none()
+            {
+                path.push(node.clone());
+                for neighbor in self.get_neighbors(node).unwrap()
+                {
+                    stack.push(neighbor.clone());
+                }
+            }
+        }
+
+        return Vec::new();
+    }
+
+    fn dfs(&self, start: T, end: T) -> Vec<T>{
+        let mut path: Vec<T> = Vec::new();
+        let mut stack: Vec<T> = Vec::new();
+        stack.push(start.clone());
+
+        while !stack.is_empty()
+        {
+            let node = stack.pop().unwrap();
+
+            if node == end
+            {
+                path.push(node);
+                return path;
+            }
+
+            if path.iter().find(|&x| *x == node).is_none()
+            {
+                path.push(node.clone());
+                for neighbor in self.get_neighbors(node).unwrap()
+                {
+                    stack.push(neighbor.clone());
+                }
+            }
+        }
+
+        return Vec::new();
+    }
+}
+
+impl<T: Clone + Eq + Hash> SearchAlgorithm<T> for Graph<T>{}
+
+pub trait GreedySearch<T>
+{
+    fn dijkstra(&self) -> Vec<T>;
+    fn bellman_ford(&self) -> Vec<T>;
+    fn floyd_warshall(&self) -> Vec<T>;
+    fn a_star(&self) -> Vec<T>;
 }
